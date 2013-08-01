47d4d4aaf0bf1c55e52e5be640a910d87f72e65e
Giso Grimm <giso.grimm@vegri.net>
2013-08-01 21:12:31 +0200

    start of time-varying time base parameter looper

---
+++ src/hos_par_looper.cc
@@ -0,0 +1,24 @@
+/*
+ * time-varying phase based parameter looping machine
+ */
+
+class parloop_t : public jackc_t {
+public:
+  parloop_t(const std::string& jname);
+  void run();
+private:
+  double looplength;
+  double uwphase;
+};
+
+
+int main(int argc,char** argv)
+{
+  if( argc < 1 )
+    throw TASCAR::ErrMsg(std::string("Usage: ")+argv[0]+" ");
+  std::string jname("parloop");
+  if( argc > 2 )
+    jname = argv[2];
+  parloop_t s("parloop");
+  s.run();
+}
