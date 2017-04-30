ba3ffab503dd86a416d961e35d7a640263900b42
Giso Grimm <giso.grimm@vegri.net>
2017-04-30 20:23:58 +0200

    more args for sendosc

--- src/hos_sendosc.cc
+++ src/hos_sendosc.cc
@@ -4,7 +4,7 @@
    \brief Send OSC messages, read from stdin
 
    \author Giso Grimm
-   \date 2011
+   \date 2011-2017
 
    \section license License (GPL)
 
@@ -62,10 +62,20 @@ int main(int argc, char** argv)
 	}else{
 	  char addr[0x4000];
 	  memset(addr,0,0x4000);
-	  float val(0.0f);
+	  float val0(0.0f);
 	  float val1(0.0f);
+	  float val2(0.0f);
+	  float val3(0.0f);
+	  float val4(0.0f);
+	  float val5(0.0f);
+	  float val6(0.0f);
+	  float val7(0.0f);
+	  float val8(0.0f);
+	  float val9(0.0f);
 	  char sval[0x4000];
-	  int narg = sscanf(rbuf,"%s %g %g",addr,&val,&val1);
+	  int narg = sscanf(rbuf,"%s %g %g %g %g %g %g %g %g %g %g",addr,
+			    &val0,&val1,&val2,&val3,&val4,
+			    &val5,&val6,&val7,&val8,&val9);
 	  switch( narg ){
 	  case 1 :
 	    narg = sscanf(rbuf,"%s %s",addr,sval);
@@ -75,10 +85,43 @@ int main(int argc, char** argv)
 	      lo_send( lo_addr, addr, "s", sval );
 	    break;
 	  case 2 :
-	    lo_send( lo_addr, addr, "f", val);
+	    lo_send( lo_addr, addr, "f", val0);
 	    break;
 	  case 3 :
-	    lo_send( lo_addr, addr, "ff", val, val1);
+	    lo_send( lo_addr, addr, "ff", 
+		     val0, val1);
+	    break;
+	  case 4 :
+	    lo_send( lo_addr, addr, "fff", 
+		     val0, val1, val2);
+	    break;
+	  case 5 :
+	    lo_send( lo_addr, addr, "ffff", 
+		     val0, val1, val2, val3);
+	    break;
+	  case 6 :
+	    lo_send( lo_addr, addr, "fffff", 
+		     val0, val1, val2, val3, val4 );
+	    break;
+	  case 7 :
+	    lo_send( lo_addr, addr, "ffffff", 
+		     val0, val1, val2, val3, val4, val5);
+	    break;
+	  case 8 :
+	    lo_send( lo_addr, addr, "fffffff", 
+		     val0, val1, val2, val3, val4, val5, val6);
+	    break;
+	  case 9 :
+	    lo_send( lo_addr, addr, "ffffffff", 
+		     val0, val1, val2, val3, val4, val5, val6, val7);
+	    break;
+	  case 10 :
+	    lo_send( lo_addr, addr, "fffffffff", 
+		     val0, val1, val2, val3, val4, val5, val6, val7, val8);
+	    break;
+	  case 11 :
+	    lo_send( lo_addr, addr, "ffffffffff", 
+		     val0, val1, val2, val3, val4, val5, val6, val7, val8, val9);
 	    break;
 	  }
 	}
