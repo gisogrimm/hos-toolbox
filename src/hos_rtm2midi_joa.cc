5373bda7186a1b0389901f5b3455a18ffcbb5605
Your Name <you@example.com>
2018-10-18 17:32:37 +0200

    added modified scripts hos_rtm2midi.cc and libhos_music.cc to split the midi composition into different voices that can be assigned to different instruments

---
+++ src/hos_rtm2midi_joa.cc
@@ -0,0 +1,220 @@
+/**
+   \file hos_rtm2midi.cc
+   \ingroup rtm
+ */
+/*
+  Copyright (C) 2004 Ian Esten
+    
+  This program is free software; you can redistribute it and/or modify
+  it under the terms of the GNU General Public License as published by
+  the Free Software Foundation; either version 2 of the License, or
+  (at your option) any later version.
+
+  This program is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+  GNU General Public License for more details.
+
+  You should have received a copy of the GNU General Public License
+  along with this program; if not, write to the Free Software
+  Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
+*/
+
+#include <jack/jack.h>
+#include <jack/midiport.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <string>
+#include <vector>
+#include "errorhandling.h"
+#include "libhos_music_joa.h"
+#include "osc_helper.h"
+#include <iostream>
+#include "hos_defs.h"
+
+/**
+   \ingroup rtm
+ */
+class midi_note_t {
+public:
+  midi_note_t();
+  uint8_t velocity;
+  uint8_t pitch;
+  uint32_t startcnt;
+  uint32_t lengthcnt;
+  uint32_t voice;
+  bool process;
+
+};
+
+midi_note_t::midi_note_t()
+  : velocity(0),
+    pitch(0),
+    startcnt(0),
+    lengthcnt(0),
+    voice(0),
+    process(false)
+{
+}
+
+/**
+   \ingroup rtm
+ */
+class midi_t : public TASCAR::osc_server_t
+{
+public:
+  midi_t(const std::string& name);
+  ~midi_t();
+  static int process(jack_nframes_t nframes, void *arg);
+  void process(jack_nframes_t nframes);
+  void add_note(const note_t& note);
+  static int add_note(const char *path, const char *types, lo_arg **argv, int argc, lo_message msg, void *user_data);
+  static int set_time(const char *path, const char *types, lo_arg **argv, int argc, lo_message msg, void *user_data);
+  void add_note(unsigned int voice,int pitch,unsigned int length,double time);
+  void set_time(double t);
+private:
+  std::vector<midi_note_t> notes;
+  jack_client_t *client;
+  jack_port_t *output_port;
+  double time;
+  double samples_per_brevis;
+  jack_nframes_t timecnt;
+};
+
+int midi_t::add_note(const char *path, const char *types, lo_arg **argv, int argc, lo_message msg, void *user_data)
+{
+  if( user_data && (argc == 4) )
+    ((midi_t*)user_data)->add_note(argv[0]->i,argv[1]->i,argv[2]->i,argv[3]->f);
+  return 0;
+}
+
+int midi_t::set_time(const char *path, const char *types, lo_arg **argv, int argc, lo_message msg, void *user_data)
+{
+  if( user_data && (argc == 1) && (types[0] == 'f') )
+    ((midi_t*)user_data)->set_time(argv[0]->f);
+  return 0;
+}
+
+void midi_t::set_time(double t)
+{
+  if( (t-time > 0.5) && (timecnt > 16000) ){
+    samples_per_brevis = 0.8*(double)timecnt/(t-time);
+    //DEBUG(samples_per_brevis);
+    timecnt = 0;
+    time = t;
+  }
+}
+
+void midi_t::add_note(unsigned int voice,int pitch,unsigned int length,double time)
+{
+  note_t n;
+  n.voice=voice;
+  n.pitch = pitch;
+  n.length = length;
+  n.time = time;
+  add_note(n);
+}
+
+
+void midi_t::add_note(const note_t& n)
+{
+  if( n.pitch == PITCH_REST )
+    return;
+  for(std::vector<midi_note_t>::iterator note=notes.begin();note!=notes.end();++note)
+    if( !(note->process) ){
+      note->startcnt = 192000;
+      note->lengthcnt = n.duration()*samples_per_brevis;
+      note->pitch = n.pitch+64;
+      note->velocity = 32;
+      note->voice = n.voice;
+      note->process = true;
+      std::cout << n << std::endl;
+      return;
+    }
+}
+
+int midi_t::process(jack_nframes_t nframes, void *arg)
+{
+  ((midi_t*)arg)->process(nframes);
+  return 0;
+}
+
+void midi_t::process(jack_nframes_t nframes)
+{
+  void* port_buf = jack_port_get_buffer(output_port, nframes);
+  jack_midi_clear_buffer(port_buf);
+//std::cout << output_port << std::endl;
+//std::cout << port_buf << std::endl;
+  unsigned char* buffer;
+  for(jack_nframes_t t=0; t<nframes; t++){
+    timecnt++;
+    for(std::vector<midi_note_t>::iterator note=notes.begin();note!=notes.end();++note)
+      if( note->process ){
+        if( note->startcnt ){
+          note->startcnt--;
+          if( !(note->startcnt) ){
+            buffer = jack_midi_event_reserve(port_buf, t, 3);
+            buffer[2] = note->velocity;		/* velocity */
+            buffer[1] = note->pitch;
+      //std::cout << n << std::endl;
+            buffer[0] = 0x90+note->voice;/* note on */
+          }
+        }else{
+          if( note->lengthcnt ){
+            note->lengthcnt--;
+            if( !(note->lengthcnt) ){
+	      buffer = jack_midi_event_reserve(port_buf, t, 3);
+	      buffer[2] = note->velocity;		/* velocity */
+	      buffer[1] = note->pitch;	/* note off */
+              buffer[0] = 0x80+note->voice;/* note on */
+            }
+          }else{;
+            note->process = false;
+          }
+        }
+      }
+  }
+}
+
+midi_t::midi_t(const std::string& name)
+  : TASCAR::osc_server_t("239.255.1.7","9877"),
+    time(0),
+    samples_per_brevis(48000),
+    timecnt(0)
+{
+  notes.resize(512);
+  if((client = jack_client_open (name.c_str(), JackNullOption, NULL)) == 0)
+    throw TASCAR::ErrMsg("Unable to open client. jack server not running?");
+  jack_set_process_callback (client, process, this);
+  output_port = jack_port_register (client, "out", JACK_DEFAULT_MIDI_TYPE, JackPortIsOutput, 0);
+  if (jack_activate(client))
+    throw TASCAR::ErrMsg("Cannot activate client.");
+ 
+  add_method("/time","f",midi_t::set_time,this);
+  add_method("/note","iiif",midi_t::add_note,this);
+  osc_server_t::activate();
+}
+
+midi_t::~midi_t()
+{
+  osc_server_t::deactivate();
+  jack_deactivate(client);
+  jack_client_close(client);
+}
+
+int main(int narg, char **args)
+{
+  midi_t midi("hos_rtm2midi");
+  while(true)
+    sleep(1);
+}
+
+/*
+ * Local Variables:
+ * mode: c++
+ * c-basic-offset: 2
+ * indent-tabs-mode: nil
+ * compile-command: "make -C .."
+ * End:
+ */
---
+++ src/libhos_music_joa.h
@@ -0,0 +1,103 @@
+#ifndef LIBHOS_MUSIC_H
+#define LIBHOS_MUSIC_H
+
+#include <iostream>
+#include <string>
+#include <vector>
+#include <stdint.h>
+
+int32_t wrapped_pitch(int32_t pitch);
+bool is_in_scale(int32_t pitch, int32_t key);
+int32_t closest_key(int32_t pitch, int32_t key);
+std::string notename(int32_t pitch);
+uint32_t closest_length(double len);
+double duration(uint32_t len);
+double frac(double x,double eps=8192.0);
+
+#define PITCH_REST 1000
+#define MAXLEN 12
+
+/**
+   \ingroup rtm
+ */
+class note_t {
+public:
+  note_t();
+  note_t(int32_t p,uint32_t len, double time = 0);
+  int32_t pitch;
+  uint32_t length;
+  uint32_t voice;
+  double time;
+  double end_time() const;
+  double duration() const;
+  friend std::ostream& operator<<(std::ostream& o, const note_t& p){  o << "[" << p.pitch << ":"<<p.length << "@" << p.time << "]"; return o;};
+};
+
+/**
+   \ingroup rtm
+*/
+class keysigchange_t {
+public:
+  keysigchange_t();
+  keysigchange_t(int32_t f,bool p);
+  keysigchange_t(uint32_t hash);
+  uint32_t hash() const;
+  int32_t fifths;
+  bool parallel;
+};
+
+/**
+   \ingroup rtm
+ */
+class keysig_t {
+public:
+  enum mode_t {
+    major, minor
+  };
+  keysig_t();
+  keysig_t(const std::string& name);
+  keysig_t(uint32_t hash);
+  keysig_t(int p, mode_t m);
+  int32_t pitch() const;
+  void setpitch(int p,mode_t m);
+  int32_t fifths;
+  mode_t mode;
+  std::string name() const;
+  friend std::ostream& operator<<(std::ostream& o, const keysig_t& p){  o << p.name(); return o;};
+  bool operator==(const keysig_t& o) const { return (fifths==o.fifths) && (mode==o.mode);};
+  keysig_t& operator+=(const keysigchange_t&);
+  uint32_t hash() const;
+};
+
+/**
+   \brief A class to deal with bars and beats based on a semi-contiuous time
+   \ingroup rtm
+*/
+class time_signature_t {
+public:
+  time_signature_t();
+  time_signature_t(uint32_t num,uint32_t denom,double startt,uint32_t addb);
+  time_signature_t(uint32_t hash_);
+  uint32_t hash() const;
+  uint32_t numerator;
+  uint32_t denominator;
+  double starttime;///< Start time from which on the new time signature is value
+  uint32_t addbar;///< Number of bars which were counted before the current time signature
+  double bar(double time) const;
+  double beat(double time) const;
+  double time(double bar) const;
+  bool unmeasured() const {return numerator==0;};
+  friend std::ostream& operator<<(std::ostream& o, const time_signature_t& p){  o << "[time " << p.numerator << "/"<<p.denominator << "@" << p.starttime << "+" << p.addbar << "]"; return o;};
+  bool operator==(const time_signature_t& o) const { return (numerator==o.numerator) && (denominator==o.denominator);};
+};
+
+#endif
+
+/*
+ * Local Variables:
+ * mode: c++
+ * c-basic-offset: 2
+ * indent-tabs-mode: nil
+ * compile-command: "make -C .."
+ * End:
+ */
